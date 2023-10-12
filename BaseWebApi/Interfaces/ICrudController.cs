c246800e972c2b250a0d6c64e2aa0903763c4a7b
SaveFeelix <felixhake@web.de>
2023-10-12 20:45:22 +0200

    Create ICrudController

---
+++ BaseWebApi/Interfaces/ICrudController.cs
@@ -0,0 +1,14 @@
+﻿using BaseWebApi.Result;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseWebApi.Interfaces
+{
+    public interface ICrudController<TGetDto, TCreateDto, TUpdateDto>
+    {
+        Task<ActionResult<BaseResult<List<TGetDto>>>> Get();
+        Task<ActionResult<BaseResult<TGetDto>>> Get(int id);
+        Task<ActionResult<BaseResult<TGetDto>>> Post(TCreateDto dto);
+        Task<ActionResult<BaseResult<TGetDto>>> Put(int id, TUpdateDto dto);
+        Task<ActionResult<BaseResult>> Delete(int id);
+    }
+}
