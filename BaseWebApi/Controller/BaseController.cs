248eb366b732ae6bfb781ee28376e849a80b4f64
SaveFeelix <felixhake@web.de>
2023-10-12 20:44:47 +0200

    Create BaseControllers

---
+++ BaseWebApi/Controller/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Net.Mime;
+
+namespace BaseWebApi.Controller
+{
+    [Authorize]
+    [ApiController]
+    [Route("Global/[controller]/[action]")]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class BaseController<TDatabase, TController> : ControllerBase where TDatabase : DbContext
+    {
+        public BaseController(TDatabase database, ILogger<TController> logger)
+        {
+            Database = database;
+            Logger = logger;
+        }
+
+        protected TDatabase Database { get; }
+        protected ILogger<TController> Logger { get; }
+
+    }
+}
