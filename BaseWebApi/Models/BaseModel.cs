15a64e4e340662521bad4a4b9c878112a73e5595
SaveFeelix <felixhake@web.de>
2023-10-13 21:05:05 +0200

    Add Undeletable property to BaseModel

--- BaseWebApi/Models/BaseModel.cs
+++ BaseWebApi/Models/BaseModel.cs
@@ -6,6 +6,7 @@ public class BaseModel
 {
     public int Id { get; set; }
     public int Version { get; set; } = 1;
+    public bool Undeletable { get; set; } = false;
     public State State { get; set; } = State.Active;
     public DateTime Created { get; set; } = DateTime.UtcNow;
     public DateTime Changed { get; set; } = DateTime.UtcNow;
