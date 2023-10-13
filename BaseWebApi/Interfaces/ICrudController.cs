7b40a623a1fa93a9be603a4559cb7f14045558d7
SaveFeelix <felixhake@web.de>
2023-10-13 17:50:12 +0200

    Rename "All" method

--- BaseWebApi/Interfaces/ICrudController.cs
+++ BaseWebApi/Interfaces/ICrudController.cs
@@ -3,9 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BaseWebApi.Interfaces
 {
-    public interface ICrudController<TGetDto, TCreateDto, TUpdateDto>
+    public interface ICrudController<TGetDto, in TCreateDto, in TUpdateDto>
     {
-        Task<ActionResult<BaseResult<List<TGetDto>>>> Get();
+        Task<ActionResult<BaseResult<List<TGetDto>>>> All();
         Task<ActionResult<BaseResult<TGetDto>>> Get(int id);
         Task<ActionResult<BaseResult<TGetDto>>> Post(TCreateDto dto);
         Task<ActionResult<BaseResult<TGetDto>>> Put(int id, TUpdateDto dto);
